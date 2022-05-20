ae9caff349209f80104752cfa1f509b158c758ae
Divyanshu-Rout <divyanshukumarrout@gmail.com>
2022-05-20 10:54:07 +0530

    error-dialogue-box

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,8 @@ import 'dart:developer' as devtools show log;
 
 import 'package:mynotes/constants/routes.dart';
 
+import '../utilities/show_error_dialog.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
 
@@ -66,13 +68,26 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('user not found');
+                  await showErrorDialogue(
+                    context,
+                    'User not Found',
+                  );
                 } else if (e.code == 'wrong-password') {
-                  devtools.log('wrong password');
+                  await showErrorDialogue(
+                    context,
+                    'wrong password',
+                  );
+                } else {
+                  await showErrorDialogue(
+                    context,
+                    'Error : ${e.code}',
+                  );
                 }
               } catch (e) {
-                devtools.log('something bad happened');
-                devtools.log(e.runtimeType.toString());
+                await showErrorDialogue(
+                  context,
+                  e.runtimeType.toString(),
+                );
               }
             },
             child: const Text('Login'),
