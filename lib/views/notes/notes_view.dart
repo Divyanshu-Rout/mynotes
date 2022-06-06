814f31bd5d1a8be51a794e34aa4fee4a668e2094
Divyanshu-Rout <divyanshukumarrout@gmail.com>
2022-06-06 17:54:08 +0530

    auth_bloc_added_to_views

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'dart:developer' as devtools show log;
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/enums/menu_actions.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storage.dart';
 import 'package:mynotes/views/notes/notes_list_view.dart';
@@ -42,9 +45,9 @@ class _NotesViewState extends State<NotesView> {
               case MenuAction.logout:
                 final shouldLogout = await showLogOutDialog(context);
                 if (shouldLogout) {
-                  await AuthService.firebase().logOut();
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil(loginRoute, (_) => false);
+                  context.read<AuthBloc>().add(
+                        const AuthEventLogOut(),
+                      );
                 }
             }
           }, itemBuilder: (context) {
